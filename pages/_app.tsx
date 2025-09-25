ae736ef50013bf0473acdc8ba64c9dcfe1f24eb1
lawrence.anthony <lawrence.anthony@cheersin.com>
2025-09-25 09:50:48 +0530

    adding page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,15 @@ import "@n8n/chat/style.css";
 import "../styles/n8n-chat-custom.css";
 import type { AppProps } from "next/app";
 import "../lib/i18n";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Juice - Insurance Claims Solutions</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
